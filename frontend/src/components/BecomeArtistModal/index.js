ffd0b9e5864e846f4aec62aaf0b2e228f6443c9e
C-Carroll <crcarroll00@gmail.com>
2024-04-20 19:57:52 -0500

    fixed fonts for modals and data entry

--- frontend/src/components/BecomeArtistModal/index.js
+++ frontend/src/components/BecomeArtistModal/index.js
@@ -48,6 +48,7 @@ const BecomeArtist = () => {
         <label>
           Artist Name:
           <input
+            className="artDes"
             type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
